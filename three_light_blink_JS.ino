4ab55c56795a11060f7e4b891e54364d483f1c7d
Jsmith15198 <77761161+Jsmith15198@users.noreply.github.com>
2022-10-10 15:38:34 -0400

    Add three_light_blink_JS.ino
    Added the code from the microcontroller programming assignment for 3 LEDs to the repository for the Git Repository setup.

---
+++ three_light_blink_JS.ino
@@ -0,0 +1,30 @@
+#define toggle 9
+#define LED1 13
+#define LED2 12
+#define LED3 11
+
+// Setup function
+void setup() {
+  pinMode(toggle, INPUT); // SPST is an input
+  pinMode(LED1, OUTPUT); // LED1 is an output
+  pinMode(LED2, OUTPUT); // LED2 is an output
+  pinMode(LED3, OUTPUT); // LED3 is an output
+}
+
+// Loop 3 LEDs turing ON and OFF based on toggle
+void loop() {
+  if(digitalRead(toggle) == HIGH) {
+    digitalWrite(LED1, HIGH); // LED1 turns ON when toggle is pressed
+    delay(1000); // Wait 1 second
+    digitalWrite(LED1, LOW); // LED1 turns OFF when toggle is pressed
+    delay(1000); // Wait 1 second
+    digitalWrite(LED2, HIGH); // LED2 turns ON when toggle is pressed
+    delay(1000); // Wait 1 second
+    digitalWrite(LED2, LOW); // LED2 turns OFF when toggle is pressed
+    delay(1000); // Wait 1 second
+    digitalWrite(LED3, HIGH); // LED3 turns ON when toggle is pressed
+    delay(1000); // Wait 1 second
+    digitalWrite(LED3, LOW); // LED3 turns OFF when toggle is pressed
+    delay(1000); // Wait 1 second
+  }
+}
